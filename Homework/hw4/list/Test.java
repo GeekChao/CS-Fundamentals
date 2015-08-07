180d660d5ecdcb4c0f53f3d8f3d8cf7a15da0c1c
Garnett <mingchao918@gmail.com>
2015-08-07 21:11:21 +0800

    Homework 6 Finished

--- Homework/hw4/list/Test.java
+++ Homework/hw4/list/Test.java
@@ -9,32 +9,33 @@ public static void main(String[] args){
     l1.insertBack(new String("3"));
     l1.insertBack(new String("4"));
     l1.insertBack(new String("5"));
-    System.out.println(l1.toString());
-    System.out.println("Front: " + l1.front().item);
-    System.out.println("Back: " + l1.back().item);
+    System.out.println("List: " + l1.toString());
+    System.out.println("The front of list: " + l1.front().item);
+    System.out.println("The back of list: " + l1.back().item);
     System.out.println("the item of Node after node2 is: " + l1.next(l1.next(l1.next(l1.head))).item);
     System.out.println("the item of Node before node2 is: " + l1.prev(l1.next(l1.next(l1.head))).item);
     l1.insertBefore(new String("10"), l1.next(l1.next(l1.head)));
-    System.out.println("insertFront: " + l1.toString());
+    System.out.println("insertFront based on node2 in the list: " + l1.toString());
     l1.insertAfter(new String("11"), l1.next(l1.next(l1.head)));
-    System.out.println("insertBack: " + l1.toString());
+    System.out.println("insertBack based on node2 in the list: " + l1.toString());
     l1.remove(l1.next(l1.next(l1.head)));
-    System.out.println("remove: " + l1.toString());
-    System.out.println("length: " + l1.size);
+    System.out.println("remove based on node2 in the list: " + l1.toString());
+    System.out.println("the length of list: " + l1.size);
 
+    System.out.println();
     /*LockDList*/
     LockDList l2 = new LockDList();
     l2.insertAfter(new String("10"), l2.head);
     l2.insertAfter(new String("11"), l2.head);
     l2.insertAfter(new String("12"), l2.head);
     /*Lock*/
-    System.out.println("Lock Befor calling remove:" + l2.toString());
+    System.out.println("LockDList befor calling remove:" + l2.toString());
     l2.remove(l2.next(l2.next(l2.head)));
     System.out.println("Lock After calling remove:" + l2.toString());
     /*Unlock*/
     LockDListNode pNode = (LockDListNode)l2.next(l2.next(l2.head));
     pNode.status = false;
-    System.out.println("Unlock Befor calling remove:" + l2.toString());
+    System.out.println("LockDList befor calling remove:" + l2.toString());
     l2.remove(pNode);
     System.out.println("unlock After calling remove:" + l2.toString());
   }
---
+++ Homework/hw6/dict/HashTableChained.java
@@ -0,0 +1,267 @@
+/* HashTableChained.java */
+
+package dict;
+import list.*;
+
+/**
+ *  HashTableChained implements a Dictionary as a hash table with chaining.
+ *  All objects used as keys must have a valid hashCode() method, which is
+ *  used to determine which bucket of the hash table an entry is stored in.
+ *  Each object's hashCode() is presumed to return an int between
+ *  Integer.MIN_VALUE and Integer.MAX_VALUE.  The HashTableChained class
+ *  implements only the compression function, which maps the hash code to
+ *  a bucket in the table's range.
+ *
+ *  DO NOT CHANGE ANY PROTOTYPES IN THIS FILE.
+ **/
+
+public class HashTableChained implements Dictionary {
+
+  /**
+   *  Place any data fields here.
+   **/
+  private SList[] entry_list;
+  private int N;
+  private int size;
+  private int collisions;
+
+  private void getMaxPrime(int num){
+    if(num < 2){
+      N = 3;
+      return;
+    }
+
+    int i;
+    for(i = num - 1; i >= 2; i--)
+      if(num % i == 0) break;
+
+    if(i == 1){
+      N = num;
+      return;
+    }
+    else{
+      getMaxPrime(--num);
+    }
+  }
+
+  private void initArray(){
+    for(int i = 0; i < N; i++)
+      entry_list[i] = new SList();
+  }
+  /** 
+   *  Construct a new empty hash table intended to hold roughly sizeEstimate
+   *  entries.  (The precise number of buckets is up to you, but we recommend
+   *  you use a prime number, and shoot for a load factor between 0.5 and 1.)
+   **/
+
+  public HashTableChained(int sizeEstimate) {
+    // Your solution here.
+    getMaxPrime(sizeEstimate);
+    entry_list = new SList[N];
+    initArray();
+  }
+
+  /** 
+   *  Construct a new empty hash table with a default size.  Say, a prime in
+   *  the neighborhood of 100.
+   **/
+
+  public HashTableChained() {
+    // Your solution here.
+    N = 97;
+    entry_list = new SList[N];
+    initArray();
+  }
+
+  /**
+   *  Converts a hash code in the range Integer.MIN_VALUE...Integer.MAX_VALUE
+   *  to a value in the range 0...(size of hash table) - 1.
+   *
+   *  This function should have package protection (so we can test it), and
+   *  should be used by insert, find, and remove.
+   **/
+
+  int compFunction(int code) {
+    // Replace the following line with your solution.
+    return code % N;
+  }
+
+  /** 
+   *  Returns the number of entries stored in the dictionary.  Entries with
+   *  the same key (or even the same key and value) each still count as
+   *  a separate entry.
+   *  @return number of entries in the dictionary.
+   **/
+
+  public int size() {
+    // Replace the following line with your solution.
+    return size;
+  }
+
+  /** 
+   *  Tests if the dictionary is empty.
+   *
+   *  @return true if the dictionary has no entries; false otherwise.
+   **/
+
+  public boolean isEmpty() {
+    // Replace the following line with your solution.
+    if(size == 0){
+      return true;
+     }else{
+      return false;
+    }
+  }
+
+  /**
+   *  Create a new Entry object referencing the input key and associated value,
+   *  and insert the entry into the dictionary.  Return a reference to the new
+   *  entry.  Multiple entries with the same key (or even the same key and
+   *  value) can coexist in the dictionary.
+   *
+   *  This method should run in O(1) time if the number of collisions is small.
+   *
+   *  @param key the key by which the entry can be retrieved.
+   *  @param value an arbitrary object.
+   *  @return an entry containing the key and value.
+   **/
+
+  public Entry insert(Object key, Object value) {
+    // Replace the following line with your solution.
+    Entry entry = new Entry(key, value);
+    int bucket = compFunction(key.hashCode());
+    //System.out.println(""+ bucket);
+    entry_list[bucket].insertBack(entry);
+
+    size++;
+    return entry;
+  }
+
+  /** 
+   *  Search for an entry with the specified key.  If such an entry is found,
+   *  return it; otherwise return null.  If several entries have the specified
+   *  key, choose one arbitrarily and return it.
+   *
+   *  This method should run in O(1) time if the number of collisions is small.
+   *
+   *  @param key the search key.
+   *  @return an entry containing the key and an associated value, or null if
+   *          no entry contains the specified key.
+   **/
+
+  public Entry find(Object key){
+    // Replace the following line with your solution.
+    int bucket = compFunction(key.hashCode());
+    if(!entry_list[bucket].isEmpty()){
+      ListNode node = entry_list[bucket].front();
+      try{
+        return (Entry)node.item();
+      }catch(InvalidNodeException e){
+        System.out.println("Invaild Node");
+      }
+      /*while(node.isValidNode()){
+        try{
+          if(equals(node.item())){
+            return (Entry)node.item();
+          }else{
+            node = node.next();
+          }
+        }catch(InvalidNodeException e){
+          System.out.println("Invaild Node");
+        }
+      }*/
+    }
+      
+    return null;
+  }
+
+  /** 
+   *  Remove an entry with the specified key.  If such an entry is found,
+   *  remove it from the table and return it; otherwise return null.
+   *  If several entries have the specified key, choose one arbitrarily, then
+   *  remove and return it.
+   *
+   *  This method should run in O(1) time if the number of collisions is small.
+   *
+   *  @param key the search key.
+   *  @return an entry containing the key and an associated value, or null if
+   *          no entry contains the specified key.
+   */
+
+  public Entry remove(Object key) {
+    // Replace the following line with your solution.
+    int bucket = compFunction(key.hashCode());
+    if(!entry_list[bucket].isEmpty()){
+      ListNode node = entry_list[bucket].front();
+      try{
+        Entry entry = (Entry)node.item();
+        node.remove();
+        size--;
+        return entry;
+      }catch(InvalidNodeException e){
+        System.out.println("Invaild Node");
+      }
+      /*while(node.isValidNode()){
+        try{
+          if(key.equals(node.item())) {
+            Entry entry = (Entry)node.item();
+            node.remove();
+            size--;
+            return entry;
+          }else{
+            node = node.next();
+          }
+        }catch(InvalidNodeException e){
+          System.out.println("Invaild Node");
+        }
+      }*/
+    }
+    
+    return null;
+  }
+
+  /**
+   *  Remove all entries from the dictionary.
+   */
+  public void makeEmpty() {
+    // Your solution here.
+    ListNode pNode;
+    for(int i = 0; i < N; i++){
+      if(!entry_list[i].isEmpty()){
+         ListNode node = entry_list[i].front(); 
+         while(node.isValidNode()){ 
+          try{
+            pNode = node.next();
+            node.remove();
+            node = pNode;
+          }catch(InvalidNodeException e){
+            System.out.println("Invaild Node");
+          }
+        }
+      }
+    }
+    size = 0;
+    collisions = 0;
+  }
+
+  public void showTable(){
+    collisions = 0;
+    for(int i = 0; i < N; i++){
+      if(!entry_list[i].isEmpty()){
+        String str = "HashTable" + "[" + i + "]: ";
+        ListNode node = entry_list[i].front();
+        while(node.isValidNode()){ 
+          try{
+            str += ((Integer)((Entry)node.item()).value()).intValue() + ", ";
+            node = node.next();
+          }catch(InvalidNodeException e){
+            System.out.println(e);
+          }
+        }  
+        System.out.println(str);  
+        collisions += entry_list[i].length() - 1; 
+      }
+    }
+    System.out.println("number of collisions:" + collisions);
+  }
+}
