f7787b3db34da0a4adb2c570473aab1d42b4fb2e
Garnett <mingchao918@gmail.com>
2015-08-18 15:46:10 +0800

    Lab12 Finished

---
+++ Homework/lab9/Proofs_files/embed.js
@@ -0,0 +1,11 @@
+<HTML>
+	<HEAD>
+		<TITLE>Service Unavailable - Connection Error</TITLE>
+		<META http-equiv=Content-Type content="text-html; charset=UTF-8">
+	</HEAD>
+	<BODY>
+		<H1>Service Unavailable</H1>
+		Connection reset by peer
+		<P>Reference #54.b7d92348.c71b4fc4.1438999500</P>
+	</BODY>
+</HTML>
