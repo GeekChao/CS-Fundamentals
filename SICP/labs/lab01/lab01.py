c747e24cd4904057aa2442164c2d34622b013ce4
Kevin Zou <kevinzou19@gmail.com>
2019-07-20 16:16:15 -0400

    finish lab 1 & 2

---
+++ SICP/labs/lab01/lab01.py
@@ -0,0 +1,68 @@
+"""Lab 1: Expressions and Control Structures"""
+
+# Coding Practice
+
+def repeated(f, n, x):
+    """Returns the result of composing f n times on x.
+
+    >>> def square(x):
+    ...     return x * x
+    ...
+    >>> repeated(square, 2, 3)  # square(square(3)), or 3 ** 4
+    81
+    >>> repeated(square, 1, 4)  # square(4)
+    16
+    >>> repeated(square, 6, 2)  # big number
+    18446744073709551616
+    >>> def opposite(b):
+    ...     return not b
+    ...
+    >>> repeated(opposite, 4, True)
+    True
+    >>> repeated(opposite, 5, True)
+    False
+    >>> repeated(opposite, 631, 1)
+    False
+    >>> repeated(opposite, 3, 0)
+    True
+    """
+    k, arg = 0, x
+    while k < n:
+        k, arg = k + 1, f(arg)
+
+    return arg
+
+def sum_digits(n):
+    """Sum all the digits of n.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    """
+    sum = 0
+    while n: 
+        sum += n % 10
+        n = n // 10
+    
+    return sum
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    return '88' in str(n)
