ae46ee5ae28f79484c869abb4c6bced5532cf0aa
Kevin Zou <kevinzou19@gmail.com>
2019-09-17 07:18:18 -0400

    do lab12

--- SICP/labs/lab12/lab12.sql
+++ SICP/labs/lab12/lab12.sql
@@ -2,16 +2,27 @@
 .read fa17data.sql
 
 CREATE TABLE obedience AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, denero, hilfinger	
+    FROM students;
 
 CREATE TABLE smallest_int AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time, smallest
+    FROM students
+    WHERE smallest > 18
+    ORDER BY smallest
+    LIMIT 20;
 
 CREATE TABLE greatstudents AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT s.date, s.color, s.pet, s.number, f.number
+    FROM students AS s, sp17students AS f
+    WHERE s.date = f.date and s.color = f.color and s.pet = f.pet;
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT s.seven 
+    FROM students AS s, checkboxes AS c
+    WHERE s.time = c.time and s.number = 7 and c.'7' = 'True';
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT m1.pet, m1.song, m1.color, m2.color 
+    FROM students AS m1, students AS m2
+    WHERE m1.time < m2.time AND m1.pet = m2.pet AND m1.song = m2.song;
